0dfb63cd8592f3e3b58290f166b3c6b58a98ff24
nsdNite <nsd7night@gmail.com>
2024-02-27 21:14:08 +0200

    SNAPSHOT

--- movie_service/urls.py
+++ movie_service/urls.py
@@ -2,25 +2,25 @@ from django.urls import path
 
 from movie_service.views import (
     IndexView,
-    MovieDetailViewFront,
-    MovieListViewFront,
+    MovieView,
+    MovieDeleteView,
+    MovieUpdateView,
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("movies/", MovieListViewFront.as_view(), name="movie-list"),
-    path(
-        "movies/<int:pk>/", MovieDetailViewFront.as_view(), name="movie-detail"
-    ),
+    path("movies/", MovieView.as_view(), name="movie-list"),
+    path("movies/<int:pk>/", MovieView.as_view(), name="movie-detail"),
+    path("movies/new/", MovieView.as_view(), name="movie-new"),
     path(
         "movies/<int:pk>/update/",
-        MovieDetailViewFront.as_view(),
-        name="movie_update",
+        MovieUpdateView.as_view(),
+        name="movie-update",
     ),
     path(
         "movies/<int:pk>/delete/",
-        MovieDetailViewFront.as_view(),
-        name="movie_delete",
+        MovieDeleteView.as_view(),
+        name="movie-delete",
     ),
 ]
 
--- movie_service/views.py
+++ movie_service/views.py
@@ -1,13 +1,15 @@
 import json
 
-
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.core.paginator import Paginator
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.edit import BaseDeleteView, DeleteView, UpdateView
 
-
+from movie_service.forms import MovieForm
 from movie_service.models import Movie, Actor, Director
 
 
@@ -32,39 +34,29 @@ class IndexView(View):
         return render(request, self.template_name, context=context)
 
 
-class MovieListViewFront(View):
-    def get(self, request):
-        movies_list = Movie.objects.all()
-        paginator = Paginator(movies_list, 25)
-        page_number = request.GET.get("page")
-        page_obj = paginator.get_page(page_number)
-        context = {
-            "movies": page_obj,
-        }
-        return render(request, "movie_service/movie_list.html", context)
+class MovieView(View):
+    def get(self, request, pk=None):
+        if pk:
+            movie = get_object_or_404(Movie, id=pk)
+            context = {
+                "movie": movie,
+            }
+            return render(request, "movie_service/movie_detail.html", context)
+        else:
+            movies = Movie.objects.all()
+            context = {
+                "movies": movies,
+            }
+            return render(request, "movie_service/movie_list.html", context)
 
 
-class MovieDetailViewFront(View):
-    def get(self, request, pk):
-        movie = get_object_or_404(Movie, pk=pk)
-        context = {"movie": movie}
-        return render(request, "movie_service/movie_detail.html", context)
+class MovieDeleteView(DeleteView):
+    model = Movie
+    success_url = reverse_lazy("movie_service:movie-list")
+    template_name = "movie_service/movie_confirm_delete.html"
 
-    @csrf_exempt
-    def post(self, request, pk):
-        data = json.loads(request.body)
-        movie = get_object_or_404(Movie, pk=pk)
 
-        movie.title = data.get("title", movie.title)
-        movie.release_year = data.get("release_year", movie.release_year)
-        movie.director = data.get("director", movie.director)
-        movie.plot = data.get("plot", movie.plot)
-        movie.save()
-
-        return JsonResponse({"message": "Movie updated successfully"})
-
-    @csrf_exempt
-    def delete(self, request, pk):
-        movie = get_object_or_404(Movie, pk=pk)
-        movie.delete()
-        return JsonResponse({"message": "Movie deleted successfully"})
+class MovieUpdateView(UpdateView):
+    model = Movie
+    fields = ["title", "release_date", "director", "actors"]
+    success_url = reverse_lazy("movie_service/movie-list")
