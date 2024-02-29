c646f18dc6333480ff5464707f3d8f7b2cd69a8d
nsdNite <nsd7night@gmail.com>
2024-02-29 22:53:04 +0200

    add https://movie-database-y78h.onrender.com to allowed_hosts

--- movie_api_test/settings.py
+++ movie_api_test/settings.py
@@ -20,7 +20,12 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "0.0.0.0",
+    "https://movie-database-y78h.onrender.com",
+]
 
 # Application definition
 
