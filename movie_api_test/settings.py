364ea12078abf62c4a57dde2853acf4e42cfa757
nsdNite <nsd7night@gmail.com>
2024-02-29 22:13:54 +0200

    add build scripts, update requirements

--- movie_api_test/settings.py
+++ movie_api_test/settings.py
@@ -15,10 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-k%08f#6x0n8)@1f*yn=u&&8t(44942o&$zfr&y-yplkwp8lh@c",
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
